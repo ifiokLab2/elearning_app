b1c1573b224e5e5c6fa9db206e35509d389119e3
ifiokLab2 <ifiokudoh6@gmail.com>
2025-02-21 11:24:11 +0100

    aaa

--- src/components/api-url.js
+++ src/components/api-url.js
@@ -1,4 +1,4 @@
 
 //const apiUrl = ' http://127.0.0.1:8000';
-const apiUrl = 'https://elearning-job-backend.onrender.com';
+const apiUrl = 'https://elearning-job-backend-a0ic.onrender.com';
 export default apiUrl;
